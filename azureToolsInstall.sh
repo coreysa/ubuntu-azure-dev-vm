43bbe21e3a0935e31723748c5378677059282563
coreysa <corey.sanders@microsoft.com>
2015-03-15 14:18:05 -0700

    Updating scripts.

---
+++ azureToolsInstall.sh
@@ -0,0 +1,12 @@
+# Installing Node.js for newer distros
+# sudo apt-get install nodejs-legacy
+# sudo apt-get install -y npm
+# sudo npm install -g azure-cli
+
+# Installing Node.js for older distors
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
+sudo npm install -g azure-cli
+
